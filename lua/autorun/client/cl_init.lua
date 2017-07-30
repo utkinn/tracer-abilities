b031f2b1c606f08a395277a62ab2046284e006fd
Nikita <shockck84@gmail.com>
2017-07-30 22:16:16 +0500

    Added functionality to the serverside blink() function (not finished yet)

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -8,6 +8,6 @@ function blink()
 	end
 end
 
-concommand.Add( "tracer_blink", blink, nil, "Zips horizontally through space in the direction you're moving.", FCVAR_DEMO )
+concommand.Add( "tracer_blink", blink, nil, "Zip horizontally through space in the direction you're moving.", FCVAR_DEMO )
 --bounds backward in time, returning her health, ammo and position on the map to precisely where they were a few seconds before.
 concommand.Add( "tracer_recall", recall, nil, "Bound backward in time, returning your health, ammo and position on the map to precisely where they were a few seconds before.", FCVAR_DEMO )
--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -3,6 +3,8 @@ AddCSLuaFile( "client/cl_init.lua" )
 util.AddNetworkString( "blink" )
 util.AddNetworkString( "recall" )
 
+BLINK_LENGHT = 367
+
 playersAngles = {}
 
 -- hook.Add( "InitPostEntity", "setupAngleTables", function()
@@ -21,9 +23,13 @@ hook.Add( "Move", "retrieveMovementAngles", function( player, moveData )
 end )
 
 function blink( player )
-	local moveAngles = playersAngles[ player ]
-	
-	
+	local aim = player:GetAimVector()
+	aim = aim * 367
+	while util.IsInWorld( aim ) or ents.FindInSphere( aim, 1 ) do	--Preventing blinking outside the world or inside another entity
+		aim = aim - player:GetAimVector()
+	end
+	aim.z = player:GetPos().z	--Restricting vertical movement
+	player:SetPos( aim )
 end
 
 net.Receive( "blink", function( length, player ) blink( player ) end )

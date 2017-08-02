071c3d9c2a36a398af5d04eb8a37383aceb987a5
Nikita <shockck84@gmail.com>
2017-08-02 15:34:54 +0500

    Added HUD (custom fonts aren't working now) and recall Fixed blink (it's a bit buggy anyway)

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -1,13 +1,112 @@
+materials = {
+	blink = Material("blink.png", "smooth"),
+	recall = Material("recall.png", "smooth")
+}
+
+function createFont()
+	surface.CreateFont("Overwatch", {
+		font = "bignoodletoo",
+		size = 50,
+		weight = 500,
+		blursize = 0,
+		scanlines = 0,
+		antialias = true,
+		underline = false,
+		italic = false,
+		strikeout = false,
+		symbol = false,
+		rotary = false,
+		shadow = false,
+		additive = false,
+		outline = false
+	})
+	surface.CreateFont("Overwatch 0.5x", {
+		font = "bignoodletoo",
+		size = 25,
+		weight = 500,
+		blursize = 0,
+		scanlines = 0,
+		antialias = true,
+		underline = false,
+		italic = false,
+		strikeout = false,
+		symbol = false,
+		rotary = false,
+		shadow = false,
+		additive = false,
+		outline = false
+	})
+end
+
+createFont()
+hook.Add("InitPostEntity", "createFont", createFont)
+
 blinks = 3
+canRecall = true
+
+TRANSPARENCY = 100
+
+recallSnapshots = {}
 
 function blink()
 	if blinks > 0 and LocalPlayer():Alive() and not LocalPlayer():IsFrozen() then
-		net.Start( "blink" )
+		timer.Start("restoreBlinks")
+		net.Start("blink")
 		net.SendToServer()
 		blinks = blinks - 1
 	end
 end
 
-concommand.Add( "tracer_blink", blink, nil, "Zip horizontally through space in the direction you're moving.", FCVAR_DEMO )
---bounds backward in time, returning her health, ammo and position on the map to precisely where they were a few seconds before.
-concommand.Add( "tracer_recall", recall, nil, "Bound backward in time, returning your health, ammo and position on the map to precisely where they were a few seconds before.", FCVAR_DEMO )
+function recall()
+	if not IsValid(recallSnapshots[os.time()]) then
+		print("recallSnapshots[" .. os.time() .. "] is invalid")
+		return
+	end
+	if canRecall and LocalPlayer():Alive() and not LocalPlayer():IsFrozen() then
+		timer.Simple(12, function() canRecall = true end)
+		recallRestoreMoment = os.time() + 12
+		net.Start("recall")
+			net.WriteTable(recallSnapshots[os.time()])
+		net.SendToServer()
+		canRecall = false
+	end
+end
+
+concommand.Add("tracer_blink", blink, nil, "Zip horizontally through space in the direction you're moving.", FCVAR_DEMO)
+concommand.Add("tracer_recall", recall, nil, "Bound backward in time, returning your health, ammo and position on the map to precisely where they were a few seconds before.", FCVAR_DEMO)
+
+timer.Create("restoreBlinks", 3, 0, function()
+	blinks = math.Clamp(blinks + 1, 0, 3)
+end)
+
+function drawIcon(icon, shouldBeRed, x, y)
+	surface.SetMaterial(icon)
+	if shouldBeRed then
+		surface.SetDrawColor(255, 0, 0, TRANSPARENCY)
+	else
+		surface.SetDrawColor(255, 255, 255, TRANSPARENCY)
+	end
+	surface.DrawTexturedRect(x, y, 50, 50)
+end
+
+hook.Add("HUDPaint", "drawBlinkIcon", function()
+	drawIcon(materials.blink, blinks == 0, ScrW() * 0.95, ScrH() * 0.75)
+	surface.SetFont("Overwatch")
+	if blinks == 0 then
+		surface.SetTextColor(255, 0, 0, TRANSPARENCY)
+	else
+		surface.SetTextColor(255, 255, 255, TRANSPARENCY)
+	end
+	surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.75)
+	surface.DrawText(blinks)
+end)
+
+hook.Add("HUDPaint", "drawRecallIcon", function()
+	drawIcon(materials.recall, not canRecall, ScrW() * 0.95, ScrH() * 0.85)
+	if not canRecall then
+		surface.SetFont("Overwatch 0.5x")
+		surface.SetTextColor(255, 0, 0, TRANSPARENCY)
+		surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.86)
+		surface.DrawText(recallRestoreMoment - os.time() + 1)
+	end
+end)
--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,11 +1,13 @@
-AddCSLuaFile( "client/cl_init.lua" )
+AddCSLuaFile("client/cl_init.lua")
 
-util.AddNetworkString( "blink" )
-util.AddNetworkString( "recall" )
+util.AddNetworkString("blink")
+util.AddNetworkString("recall")
 
 BLINK_LENGHT = 367
 
 playerMoveAngles = {}
+
+recallSnapshots = {}
 
 -- hook.Add( "InitPostEntity", "setupAngleTables", function()
 	-- local players = player.GetHumans()
@@ -14,43 +16,77 @@ playerMoveAngles = {}
 	-- end
 -- end )
 
-hook.Add( "PlayerDisconnected", "removeAngleTableEntry", function( player )
-	playerMoveAngles[ player ] = nil
-end )
+hook.Add("PlayerDisconnected", "removeAngleTableEntry", function(player)
+	playerMoveAngles[player] = nil
+	recallSnapshots[player] = nil
+end)
 
-hook.Add( "Move", "retrieveMovementAngles", function( player, moveData )
-	playerMoveAngles[ player ] = moveData:GetMoveAngles()
-end )
+hook.Add("PlayerConnect", "removeAngleTableEntry", function(player)
+	recallSnapshots[player] = {}
+end)
 
-function blink( player )
-	local target = playerMoveAngles[ player ]:Forward()
+hook.Add("Move", "retrieveMovementAngles", function(player, moveData)
+	playerMoveAngles[player] = moveData:GetMoveAngles()
+end)
+
+function blink(player)
+	local target = playerMoveAngles[player]:Forward()
+	--local target = player:GetPos() + playerMoveAngles[player]:Forward()
 	if target.x == 0 and target.y == 0 then
 		target = player:GetAimVector()
 	end
-	target = target * 367
+	target = target * BLINK_LENGHT
+	target = target + player:GetPos()
 	target.z = player:GetPos().z	--Restricting vertical movement
 	
 	local tr = util.TraceEntity({	--Trace and Tracer...
-		start = player:GetPos(),
+		start = player:GetPos() + Vector(0, 0, 10),
 		endpos = target,
 		filter = function()	--Trace(r) passes through all entities
 			return false
 		end
-	}, player )
+	}, player)
 	
 	if tr.Hit then
-		player:SetPos( tr.HitPos )
+		player:SetPos(tr.HitPos)
 	else
-		player:SetPos( target )
+		player:SetPos(target)
 	end
+	player:EmitSound("blink" .. math.random(1, 3) .. ".wav")
 	-- while util.IsInWorld( target ) or ents.FindInSphere( target, 1 ) do	--Preventing blinking outside the world or inside another entity
 		-- target = target - player:GetAimVector()
 	-- end
 	-- target = target - player:GetAimVector() * 32	--Guaranteed no-stuck 
-	-- 
-	
-	
 end
 
-net.Receive( "blink", function( length, player ) blink( player ) end )
-net.Receive( "recall", function( length, player ) recall( player ) end )
+function recall(player)
+	--local recallData = recallSnapshots[player]
+	local recallData = net.ReadTable()
+	player:SetHealth(recallData.health)
+	player:SetArmor(recallData.armor)
+	player:SetPos(recallData.position)
+	player:SetAngles(recallData.angles)
+	player:Extinguish()
+end
+
+-- hook.Add("InitPostEntity", "createRecallTimer", function()
+	-- timer.Create("saveRecallData", 1, 0, function()
+		-- local curTime = os.time()
+		-- for _, player in pairs(player.GetAll()) do
+			-- recallSnapshots[player][curTime + 3] =
+			-- {
+				-- health = player:Health(),
+				-- armor = player:Armor(),
+				-- position = player:GetPos(),
+				-- angles = player:GetAngles()
+			-- }
+			-- for i = 1, 5 do
+				-- table.remove(recallSnapshots[player], curTime - i)
+			-- end
+			-- print("created recallSnapshots[" .. player .. "][" .. curTime .. "]")
+		-- end
+	-- end)
+-- end)
+
+net.Receive("blink", function(length, player) blink(player) end)
+net.Receive("recall", function(length, player) recall(player) end)

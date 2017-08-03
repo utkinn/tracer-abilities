4bdb56f0f0cd3869d904cfe4a5a0266ea39e913b
Nikita <shockck84@gmail.com>
2017-08-03 18:53:56 +0500

    Moved specific stuff calculation to server

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -2,6 +2,7 @@ AddCSLuaFile("client/cl_init.lua")
 
 util.AddNetworkString("blink")
 util.AddNetworkString("recall")
+util.AddNetworkString("blip")
 
 BLINK_LENGHT = 367	--~7 meters
 snapshotTick = 0	--Number of current snapshot
@@ -10,65 +11,101 @@ recallSnapshots = {}	--Table for storing all snapshots
 
 TICK_RATE = 0.05	--Smoothness of recall.
 
-CreaterConVar("tracer_blink_adminonly", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Allow blinking to admins only.")
-CreaterConVar("tracer_recall_adminonly", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Allow recalling to admins only.")
-CreaterConVar("tracer_blink_stack", 3, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Blink stack size.")
-CreaterConVar("tracer_blink_cooldown", 3, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Cooldown of one blink in seconds.")
-CreaterConVar("tracer_recall_cooldown", 12, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Cooldown of recall in seconds.")
+CreateConVar("tracer_blink_adminonly", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Allow blinking to admins only.")
+CreateConVar("tracer_recall_adminonly", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Allow recalling to admins only.")
+CreateConVar("tracer_blink_stack", 3, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Blink stack size.")
+CreateConVar("tracer_blink_cooldown", 3, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Cooldown of one blink in seconds.")
+CreateConVar("tracer_recall_cooldown", 12, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Cooldown of recall in seconds.")
+
+hook.Add( "PlayerSpawn", "resetAbilities", function(player)
+	player:SetNWInt("blinks", 3)
+	player:SetNWBool("canRecall", true)
+end)
+
+function restoreBlinks(player)
+	if player:GetNWInt("blinks") ~= 3 then
+		player:SetNWInt("blinks", player:GetNWInt("blinks") + 1)
+		net.Start("blip")
+		net.Send(player)
+	else
+		timer.Remove("player_" .. player:UserID())
+	end
+end
 
 function blink(player)
-	local playerAngles = player:EyeAngles()
-	playerAngles.pitch = 0	--Restricting vertical movement
-
-	local blinkDirection = playerAngles:Forward()
-
-	--Direction blinks
-	if player:KeyDown( IN_MOVELEFT ) then blinkDirection = -playerAngles:Right() end
-	if player:KeyDown( IN_MOVERIGHT ) then blinkDirection = playerAngles:Right() end
-	if player:KeyDown( IN_BACK ) then blinkDirection = -playerAngles:Forward() end
-
-	blinkDirection = player:GetPos() + blinkDirection * BLINK_LENGHT
-	
-	local tr = util.TraceEntity({	--Trace and Tracer...
-		start = player:GetPos() + Vector(0, 0, 10),
-		endpos = blinkDirection + Vector(0, 0, 10),
-		filter = function()	--Trace(r) passes through all entities
-			return false
+	if GetConVar("tracer_blink_adminonly"):GetBool() then
+		if not player:IsAdmin() then return end
+	end
+	if player:GetNWInt("blinks") > 0 and player:Alive() and not player:IsFrozen() then
+		if not timer.Exists("player_" .. player:UserID()) then 
+			timer.Create("restoreBlinks_" .. player:UserID(), 2, 0, function() restoreBlinks(player) end)	--Reset a cooldown timer
 		end
-	}, player)
+		local playerAngles = player:EyeAngles()
+		playerAngles.pitch = 0	--Restricting vertical movement
 	
-	player:SetPos(tr.Hit and tr.HitPos or blinkDirection)
-	player:EmitSound("blink" .. math.random(3) .. ".wav")
-	if player:GetInfoNum("tracer_callouts", 0) and math.random() < 0.3 then
-		timer.Simple(0.7, function() player:EmitSound("callouts/blink/" .. math.random(2) .. ".wav") end)
+		local blinkDirection = playerAngles:Forward()
+	
+		--Direction blinks
+		if player:KeyDown( IN_MOVELEFT ) then blinkDirection = -playerAngles:Right() end
+		if player:KeyDown( IN_MOVERIGHT ) then blinkDirection = playerAngles:Right() end
+		if player:KeyDown( IN_BACK ) then blinkDirection = -playerAngles:Forward() end
+	
+		blinkDirection = player:GetPos() + blinkDirection * BLINK_LENGHT
+		
+		local tr = util.TraceEntity({	--Trace and Tracer...
+			start = player:GetPos() + Vector(0, 0, 10),
+			endpos = blinkDirection + Vector(0, 0, 10),
+			filter = function()	--Trace(r) passes through all entities
+				return false
+			end
+		}, player)
+		
+		player:SetPos(tr.Hit and tr.HitPos or blinkDirection)
+		player:EmitSound("blink" .. math.random(3) .. ".wav")
+		if player:GetInfoNum("tracer_callouts", 0) and math.random() < 0.3 then
+			timer.Simple(0.7, function() player:EmitSound("callouts/blink/" .. math.random(2) .. ".wav") end)
+		end
+		player:SetNWInt("blinks", player:GetNWInt("blinks") - 1)
 	end
 end
 
 function recall(player)
-	local i = snapshotTick - 1
-	
-	player:GodEnable()
-	player:SetColor(Color(255, 255, 255, 0))
-	player:Lock()
-	player:EmitSound("recall.mp3")
-	
-	timer.Create("recallEffect", 1.25 / (3 / TICK_RATE), 3 / TICK_RATE, function()
-		i = i - 1
-		local recallData = recallSnapshots[i][player]
+	if GetConVar("tracer_recall_adminonly"):GetBool() then
+		if not player:IsAdmin() then return end
+	end
+	if player:GetNWBool("canRecall") and player:Alive() and not player:IsFrozen() then
+		local i = snapshotTick - 1
 		
-		player:SetHealth(recallData.health)
-		player:SetArmor(recallData.armor)
-		player:SetPos(recallData.position)
-		player:SetAngles(recallData.angles)
-		player:Extinguish()
-	end)
-	timer.Simple(1.25, function()
-		player:GodDisable()
-		player:SetColor(Color(255, 255, 255, 255))
-		player:UnLock()
-	end)
-	if player:GetInfoNum("tracer_callouts", 0) and math.random() < 0.5 then
-		timer.Simple(1.5, function() player:EmitSound("callouts/recall/" .. math.random(4) .. ".wav") end)
+		player:GodEnable()
+		player:SetColor(Color(255, 255, 255, 0))
+		player:Lock()
+		player:EmitSound("recall.mp3")
+		
+		timer.Create("recallEffect", 1.25 / (3 / TICK_RATE), 3 / TICK_RATE, function()
+			i = i - 1
+			local recallData = recallSnapshots[i][player]
+			
+			player:SetHealth(recallData.health)
+			player:SetArmor(recallData.armor)
+			player:SetPos(recallData.position)
+			player:SetAngles(recallData.angles)
+			player:Extinguish()
+		end)
+		timer.Simple(1.25, function()
+			player:GodDisable()
+			player:SetColor(Color(255, 255, 255, 255))
+			player:UnLock()
+		end)
+		if player:GetInfoNum("tracer_callouts", 0) and math.random() < 0.5 then
+			timer.Simple(1.5, function() player:EmitSound("callouts/recall/" .. math.random(4) .. ".wav") end)
+		end
+		
+		player:SetNWBool("canRecall", false)
+		timer.Simple(12, function()
+			player:SetNWBool("canRecall", true)	--Regain ability after 12 seconds
+			net.Start("blip")
+			net.Send(player)
+		end)
 	end
 end
 

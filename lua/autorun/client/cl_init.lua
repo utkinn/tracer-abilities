bbf721998914eae416f619d012b36363a263302e
Nikita <shockck84@gmail.com>
2017-08-04 10:02:49 +0500

    Added HUD concommands

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -61,47 +61,59 @@ end
 --Creating console commands
 concommand.Add("tracer_blink", blink, nil, "Zip horizontally through space in the direction you're moving.", FCVAR_DEMO)
 concommand.Add("tracer_recall", recall, nil, "Bound backward in time, returning your health, ammo and position on the map to precisely where they were a few seconds before.", FCVAR_DEMO)
-CreateClientConVar("tracer_callouts", 1, true, true, "Should your character say Tracer's phrases when you use abilities?") 
+CreateClientConVar("tracer_callouts", 1, true, true, "Should your character say Tracer's phrases when you use abilities?")
+CreateClientConVar("tracer_hud", 1, true, false, "Enable the HUD.")
+CreateClientConVar("tracer_notification_blips", 1, true, false, "Enable ability restore notification sound.")
 
 function drawIcon(icon, shouldBeRed, x, y)
-	surface.SetMaterial(icon)
-	if shouldBeRed then
-		surface.SetDrawColor(255, 48, 0, TRANSPARENCY)	--Red
-	else
-		surface.SetDrawColor(255, 208, 64, TRANSPARENCY)	--Yellow
+	if GetConVar("tracer_hud"):GetBool() then
+		surface.SetMaterial(icon)
+		if shouldBeRed then
+			surface.SetDrawColor(255, 48, 0, TRANSPARENCY)	--Red
+		else
+			surface.SetDrawColor(255, 208, 64, TRANSPARENCY)	--Yellow
+		end
+		surface.DrawTexturedRect(x, y, 50, 50)
 	end
-	surface.DrawTexturedRect(x, y, 50, 50)
 end
 
 hook.Add("HUDPaint", "drawIconBackground", function()	--Background rectangle
-	surface.SetDrawColor(0, 0, 0, 75)
-	surface.DrawRect(ScrW() * 0.92, ScrH() * 0.72, ScrW() * 0.075, ScrH() * 0.2)
+	if GetConVar("tracer_hud"):GetBool() then
+		surface.SetDrawColor(0, 0, 0, 75)
+		surface.DrawRect(ScrW() * 0.92, ScrH() * 0.72, ScrW() * 0.075, ScrH() * 0.2)
+	end
 end)
 
 hook.Add("HUDPaint", "drawBlinkIcon", function()
-	local blinks = LocalPlayer():GetNWInt("blinks")
-	drawIcon(materials.blink, blinks == 0, ScrW() * 0.95, ScrH() * 0.75)
-	surface.SetFont("Overwatch")
-	if blinks == 0 then
-		surface.SetTextColor(255, 48, 0, TRANSPARENCY)
-	else
-		surface.SetTextColor(255, 208, 64, TRANSPARENCY)
+	if GetConVar("tracer_hud"):GetBool() then
+		local blinks = LocalPlayer():GetNWInt("blinks")
+		drawIcon(materials.blink, blinks == 0, ScrW() * 0.95, ScrH() * 0.75)
+		surface.SetFont("Overwatch")
+		if blinks == 0 then
+			surface.SetTextColor(255, 48, 0, TRANSPARENCY)
+		else
+			surface.SetTextColor(255, 208, 64, TRANSPARENCY)
+		end
+		surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.75)
+		surface.DrawText(blinks)
 	end
-	surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.75)
-	surface.DrawText(blinks)
 end)
 
 hook.Add("HUDPaint", "drawRecallIcon", function()
-	local canRecall = LocalPlayer():GetNWBool("canRecall")
-	drawIcon(materials.recall, not canRecall, ScrW() * 0.95, ScrH() * 0.85)
-	if not canRecall then
-		surface.SetFont("Overwatch 0.5x")
-		surface.SetTextColor(255, 48, 0, TRANSPARENCY)
-		surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.86)
-		surface.DrawText(recallRestoreMoment - os.time() + 1)
+	if GetConVar("tracer_hud"):GetBool() then
+		local canRecall = LocalPlayer():GetNWBool("canRecall")
+		drawIcon(materials.recall, not canRecall, ScrW() * 0.95, ScrH() * 0.85)
+		if not canRecall then
+			surface.SetFont("Overwatch 0.5x")
+			surface.SetTextColor(255, 48, 0, TRANSPARENCY)
+			surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.86)
+			surface.DrawText(recallRestoreMoment - os.time() + 1)
+		end
 	end
 end)
 
 net.Receive("blip", function()
-	surface.PlaySound("buttons/blip1.wav")	--Notify user
+	if GetConVar("tracer_notification_blips"):GetBool() then
+		surface.PlaySound("buttons/blip1.wav")	--Notify user
+	end
 end)

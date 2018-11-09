1ddeca65705489e4f652ab9d6272af636cb860ba
Nikita <shockck84@gmail.com>
2018-11-09 14:26:39 +0500

    Remove Tracer Abilities old icons

--- lua/autorun/client/owta_hud.lua
+++ lua/autorun/client/owta_hud.lua
@@ -52,52 +52,6 @@ local function drawCrosshairBlinkPiece(screenYPercent, blinksRequiredToBeFilled)
     )
 end
 
-local function shouldPlayBlip()
-    return GetConVar("tracer_notification_blips"):GetBool()
-end
-
-local function playBlip()
-    surface.PlaySound("buttons/blip1.wav")
-end
-
-hook.Add("HUDPaint", "Draw icon background plate", function()
-    if not shouldDrawHUD() then return end
-
-    local BLACK_TRANSPARENT = Color(0, 0, 0, 75)
-
-    local X = ScrW() * 0.91
-    local Y = ScrH() * 0.62
-    local WIDTH = ScrW() * 0.085
-    local HEIGTH = ScrH() * 0.28
-
-    surface.SetDrawColor(BLACK_TRANSPARENT)
-    surface.DrawRect(X, Y, WIDTH, HEIGTH)
-end)
-
-hook.Add("HUDPaint", "Draw Blink icon", function()
-    if not shouldDrawHUD() then return end
-
-    local blinks = LocalPlayer():GetNWInt("blinks")
-    local ICON_X = ScrW() * 0.95
-    local ICON_Y = ScrH() * 0.65
-    local TEXT_X = ScrW() * 0.93
-    local TEXT_Y = ScrH() * 0.65
-
-    local colorToUse
-    if blinks == 0 then
-        colorToUse = RED
-    else
-        colorToUse = YELLOW
-    end
-
-    drawIcon(OWTA_MATERIALS.blink, blinks == 0, ICON_X, ICON_Y)
-
-    surface.SetFont("Overwatch")
-    surface.SetTextColor(colorToUse)
-    surface.SetTextPos(TEXT_X, TEXT_Y)
-    surface.DrawText(blinks)
-end)
-
 hook.Add("HUDPaint", "Draw crosshair Blink piece", function()
     if not shouldDrawCrosshairHUD() then return end
 
@@ -115,35 +69,4 @@ hook.Add("HUDPaint", "drawCrosshairRecall", function()
     local canRecall = LocalPlayer():GetNWBool("canRecall")
 
     drawCrosshairIcon(recallMaterials[true], recallMaterials[false], canRecall, ScrW() * 0.51, ScrH() * 0.49)
-end)
-
-hook.Add("HUDPaint", "drawRecallIcon", function()
-    if not shouldDrawHUD() then return end
-
-    local canRecall = LocalPlayer():GetNWBool("canRecall")
-
-    drawIcon(OWTA_MATERIALS.recall, not canRecall, ScrW() * 0.95, ScrH() * 0.75)
-
-    if not canRecall then
-        surface.SetFont("Overwatch 0.5x")
-        surface.SetTextColor(255, 48, 0, TRANSPARENCY)
-        surface.SetTextPos(ScrW() * 0.93, ScrH() * 0.76)
-        surface.DrawText(LocalPlayer():GetNWInt("recallRestoreTime"))
-    end
-end)
-
-hook.Add("HUDPaint", "drawBombIcon", function()
-    if not shouldDrawHUD() then return end
-
-    drawIcon(OWTA_MATERIALS.bomb, false, ScrW() * 0.95, ScrH() * 0.83)
-    surface.SetFont("Overwatch")
-    surface.SetTextColor(255, 208, 64, TRANSPARENCY)
-    surface.SetTextPos(ScrW() * 0.91, ScrH() * 0.83)
-    surface.DrawText(math.Round(LocalPlayer():GetNWInt("bombCharge", 0)) .. "%")
-end)
-
-net.Receive("blip", function()
-    if shouldPlayBlip() then
-        playBlip()
-    end
 end)
